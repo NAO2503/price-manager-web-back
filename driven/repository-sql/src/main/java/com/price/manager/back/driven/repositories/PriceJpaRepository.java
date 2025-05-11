32d5ec84c5fda6a82bf83f0d6ab209d9eb1bf17b
Napoleon Avila Ochoa <nao@NA701970-HP-ProBook-430-G8-Notebook-PC.station>
2025-05-11 14:19:28 +0200

    refactor: :recycle: Adjust sql query sintax with text block and parameters

--- driven/repository-sql/src/main/java/com/price/manager/back/driven/repositories/PriceJpaRepository.java
+++ driven/repository-sql/src/main/java/com/price/manager/back/driven/repositories/PriceJpaRepository.java
@@ -3,6 +3,7 @@ package com.price.manager.back.driven.repositories;
 import com.price.manager.back.driven.repositories.models.PriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,6 +11,17 @@ import java.util.Optional;
 
 public interface PriceJpaRepository extends JpaRepository<PriceEntity, Long> {
 
-    @Query("SELECT p FROM PriceEntity p WHERE p.brandId = ?1 AND p.productId = ?2 AND p.startDate <= ?3 AND p.endDate >= ?3 ORDER BY p.priority DESC, p.priceList DESC")
-    Optional<List<PriceEntity>> findAllByBrandIdAndProductIdBetweenDates(Long brandId, Long productId, LocalDateTime dateBetween);
+    @Query("""
+            SELECT p
+            FROM PriceEntity p
+            WHERE p.brandId = :brandId
+              AND p.productId = :productId
+              AND :dateBetween BETWEEN p.startDate AND p.endDate
+            ORDER BY p.priority DESC, p.priceList DESC
+            """)
+    Optional<List<PriceEntity>> findAllByBrandIdAndProductIdBetweenDates(
+            @Param("brandId") Long brandId,
+            @Param("productId") Long productId,
+            @Param("dateBetween") LocalDateTime dateBetween
+    );
 }
